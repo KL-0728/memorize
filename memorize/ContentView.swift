9acdef12429e8906ed01f151689f50ff4e097445
KL-0728 <likaili0728@gmail.com>
2026-03-16 18:56:44 +0800

    update program

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -9,15 +9,30 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        HStack{
+            CardView(isFaceUp: false)
+            CardView()
+            CardView()
         }
         .padding()
+        .foregroundStyle(.yellow)
     }
 }
+struct CardView: View {
+    var isFaceUp: Bool=true
+    var body: some View {
+        ZStack{
+            if isFaceUp{
+                RoundedRectangle(cornerRadius: 20)
+                    .strokeBorder(lineWidth: 3)
+                Text("bochen is gay")
+            }
+            else{
+                RoundedRectangle(cornerRadius: 20)
+            }
+            }
+        }
+    }
 
 #Preview {
     ContentView()

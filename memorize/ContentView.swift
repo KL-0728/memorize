b715d6188ad61b94032d2b31d072ff68abd32977
KL-0728 <likaili0728@gmail.com>
2026-03-23 18:05:42 +0800

    feat:新增點擊翻面

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -8,29 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    //var emojis=["A","B","C","D"]
+    var emojis:Array<String>=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    @State var emojicount=6
     var body: some View {
-        HStack{
-            CardView(isFaceUp: false)
-            CardView()
-            CardView()
+        VStack{
+            HStack{
+                ForEach(emojis[0..<emojicount],id:\.self){emoji in CardView(content: emoji)}
+            }
+            HStack{
+                Button(action:{emojicount-=1},label:{Text("Remove Card")})
+                Spacer()
+                Button(action:{emojicount+=1},label:{Text("Add Card")})
+            }
         }
         .padding()
         .foregroundStyle(.yellow)
     }
 }
 struct CardView: View {
-    var isFaceUp: Bool=true
+    @State var isFaceUp: Bool=true
+    var content: String
     var body: some View {
         ZStack{
+            let shape=RoundedRectangle(cornerRadius: 20)
             if isFaceUp{
                 RoundedRectangle(cornerRadius: 20)
-                    .strokeBorder(lineWidth: 3)
-                Text("bochen is gay")
+                    shape.fill(.white)
+                    shape.strokeBorder(lineWidth: 3)
+                Text(content)
+                    .font(.largeTitle)
             }
             else{
-                RoundedRectangle(cornerRadius: 20)
+                shape
             }
             }
+        .onTapGesture(perform: {
+            isFaceUp = !isFaceUp
+        })
         }
     }
 

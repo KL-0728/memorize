b4a09fdfc1ce1743c1877ac2a4d146d0e86361aa
KL-0728 <likaili0728@gmail.com>
2026-03-30 17:27:44 +0800

    feat:lazyVGrid

--- memorize/ContentView.swift
+++ memorize/ContentView.swift
@@ -10,20 +10,32 @@ import SwiftUI
 struct ContentView: View {
     //var emojis=["A","B","C","D"]
     var emojis:Array<String>=["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
-    @State var emojicount=6
+    @State var emojicount=20
     var body: some View {
         VStack{
-            HStack{
-                ForEach(emojis[0..<emojicount],id:\.self){emoji in CardView(content: emoji)}
-            }
-            HStack{
-                remove
-                Spacer()
-                add
-            }
+            cardList
+            Spacer()
+            actionButtons
         }
         .padding()
         .foregroundStyle(.yellow)
+    }
+    var cardList: some View{
+        ScrollView{
+            LazyVGrid(columns:[GridItem(.adaptive(minimum: 85),spacing: 0)],spacing: 0){
+                ForEach(emojis[0..<emojicount],id:\.self){emoji in CardView(content: emoji)
+                        .aspectRatio(2/3,contentMode: .fit)
+                }
+            }
+        }
+    }
+    var actionButtons: some View{
+        HStack{
+            remove
+            Spacer()
+            add
+        }
+        .font(.largeTitle)
     }
     var add: some View{
         Button{
@@ -35,6 +47,7 @@ struct ContentView: View {
                    Image(systemName: "plus.circle")
         }
     }
+    
     var remove: some View{
         Button{
             if emojicount>1{
@@ -52,22 +65,23 @@ struct CardView: View {
     var body: some View {
         ZStack{
             let shape=RoundedRectangle(cornerRadius: 20)
-            if isFaceUp{
-                RoundedRectangle(cornerRadius: 20)
-                    shape.fill(.white)
-                    shape.strokeBorder(lineWidth: 3)
+            Group
+            {
+                shape.fill(.white)
+                shape.strokeBorder(lineWidth: 3)
                 Text(content)
-                    .font(.largeTitle)
+                    .font(Font.system(size: 300))
+                    .minimumScaleFactor(0.01)
+                    .aspectRatio(1,contentMode: .fit)
             }
-            else{
-                shape
-            }
-            }
-        .onTapGesture(perform: {
+            .opacity(isFaceUp ? 1:0)
+            shape.opacity(isFaceUp ? 0:1)
+        }
+        .onTapGesture (perform:{
             isFaceUp = !isFaceUp
         })
-        }
     }
+}
 
 #Preview {
     ContentView()
